8821aed83ea70b4da27e86f632cf1effe13232ad
MohammadAmin Tabesh <amintabeshyt@gmail.com>
2024-10-17 22:32:09 +0330

    Final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ContextProvider from "./contexts/ContextProvider";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const { default: AppLayout } = require("./components/AppLayout");
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <ContextProvider>
         <AppLayout />
-      <ReactQueryDevtools initialIsOpen={false} />
       </ContextProvider>
     </QueryClientProvider>
   );
